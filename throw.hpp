ea4cf970bf10c3b3e424d4bdcaf81722e9eb8aaa
M-MoC <lonarbiterval@gmail.com>
2018-01-04 10:36:13 +0000

    throw.hpp

---
+++ throw.hpp
@@ -0,0 +1,31 @@
+ /*
+ * Example usage:
+
+#include <iostream>
+
+int main()
+{
+	try
+	{
+		mmocTHROW(std::runtime_error,"this is where you describe the error");
+	} catch(std::runtime_error& e)
+		{ std::cout<<e.what(); }
+	return 0;
+}
+
+ * would produce the output:
+
+std::runtime_error thrown at line 12 of program.cpp: this is where you describe the error
+
+ * assuming the file containing the program is saved as "program.cpp"
+*/
+
+#ifndef mmocTHROW_HPP
+#define mmocTHROW_HPP
+
+#include <string>
+
+#define mmocTHROW(exception,description) throw Exception(std::string("\n" #exception \
+	" thrown at line ")+std::to_string(__LINE__)+" of "+__FILE__+": "+description+"\n")
+
+#endif
